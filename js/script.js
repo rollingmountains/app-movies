9c060fba7404e5d7e37cc3a75f3f08fdab5700f9
rollingmountains <balakrishnapaudel@gmail.com>
2023-12-27 10:55:15 +0545

    page router and active link

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+// Highlight tab
+function highlightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+
+  links.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
+    }
+  });
+}
+
+// Initialize the app
+function init() {
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':
+      console.log('Home');
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movie Details');
+      break;
+    case '/tv-details.html':
+      console.log('Tv show details');
+      break;
+    case '/search.html':
+      console.log('Search');
+      break;
+  }
+
+  highlightActiveLink();
+}
+
+document.addEventListener('DOMContentLoaded', init);
