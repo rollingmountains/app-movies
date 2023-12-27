61e150e8be3a3cad93d981e68d2be0f2c3175985
rollingmountains <balakrishnapaudel@gmail.com>
2023-12-27 12:04:10 +0545

    display popular movies

--- js/script.js
+++ js/script.js
@@ -13,12 +13,61 @@ function highlightActiveLink() {
   });
 }
 
+// fetch popular movie to display in home page
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData('movie/popular');
+
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="/movie-details.html?id=1">
+         ${
+           movie.poster_path
+             ? `<img
+              src="https://images.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt=${movie.title}
+            />`
+             : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="Movie Title"
+            />`
+         }
+        
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">${movie.release_date}</small>
+            </p>
+          </div>
+`;
+    document.getElementById('popular-movies').appendChild(div);
+  });
+}
+
+// fetch api for TMDB
+async function fetchAPIData(endpoint) {
+  const api_key = 'a8c6b8562cb3f14c24958ab98f97b910';
+  const api_url = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(
+    `${api_url}${endpoint}?api_key=${api_key}&language=en-US`
+  );
+
+  const data = await response.json();
+
+  return data;
+}
+
 // Initialize the app
 function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('Home');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('Shows');
